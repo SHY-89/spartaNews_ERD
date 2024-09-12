722685be6df39d0d4a57b58914b1fd302966a9d3
sanghyun Lee <tkdgus1569@gmail.com>
2024-09-12 11:46:25 +0900

    feat: 회원가입 기능완료

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView
-
+from . import views
 
 urlpatterns = [
-    path("login/",  TokenObtainPairView.as_view())
+    path("login/",  TokenObtainPairView.as_view()),
+    path("signup/", views.Signup.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import User
+from rest_framework.response import Response
+import re
+
+# 회원 가입
+class Signup(APIView):
+    # 검증 과정.
+    def post(self,request):
+        username = request.data.get('username') if request.data.get('username') else ''
+        password = request.data.get('password') if request.data.get('password') else ''
+        password_ok = request.data.get('password_ok') if request.data.get('password_ok') else ''
+        error = ''
+        
+        if len(username)<5 or not re.search(r"[a-zA-Z0-9]", username):
+            return Response({'error':'영문 및 숫자만 가능. 5자 이상'},status=400)
+        if password_ok != password:
+            return Response({'error':'비밀번호가 다릅니다.'}, status=400)
+        # 비밀번호 검증
+        if not re.search(r"[a-zA-Z]", password):
+            error = "비밀번호는 하나 이상의 영문이 포함되어야 합니다."
+        if not re.search(r"\d", password):
+            error = "비밀번호는 하나 이상의 숫자가 포함되어야 합니다."
+        if not re.search(r"[!@#$%^&*()]", password):
+            error = "비밀번호는 적어도 하나 이상의 특수문자(!@#$%^&*())가 포함되어야 합니다."
+
+        if error != '':
+            return Response({'error':error},status=400)
+        
+        # 유저 검증 통과 시,
+        user=User.objects.create_user(username=username,password=password)
+        return Response(status=200)
 
 # Create your views here.
