1764006207ea55c6663f7670b9ee720621bc1c11
namjimin3123832 <mimi3123832@gmail.com>
2024-09-13 11:33:05 +0900

    comment count 추가

--- news/serializer.py
+++ news/serializer.py
@@ -2,8 +2,16 @@ from .models import Article
 from rest_framework import serializers
 
 
+
 # 게시글 목록 시리얼라이저
 class ArticleSerializer(serializers.ModelSerializer):
+    comment_count = serializers.SerializerMethodField()
+
+    def get_comment_count(self,obj):
+        return obj.comments_aticle.count()
+
+
+
     author = serializers.ReadOnlyField(
         source="User.username"
     )  # 작성자는 읽기전용으로 / 'source=' 으로 username만 가져옴
@@ -16,7 +24,11 @@ class ArticleSerializer(serializers.ModelSerializer):
             "content",
             "author",
             "created_at",
+            "comment_count",
+
         ]
         read_only_fields = ('created_at', )
+
+
 
         
