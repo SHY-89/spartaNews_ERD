91b2091ad6c20b051539c9dd7dd513fab2be5d26
namjimin3123832 <mimi3123832@gmail.com>
2024-09-13 19:25:26 +0900

    목록에 게시물 작성시간 기능 구현( ex. n년 전, n 달 전, n주 전,,,)

--- news/serializer.py
+++ news/serializer.py
@@ -1,15 +1,25 @@
 from .models import Article, Comment
 from rest_framework import serializers
-
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from django.utils import timezone
 
 
 # 게시글 목록 시리얼라이저
 class ArticleSerializer(serializers.ModelSerializer):
     comment_count = serializers.SerializerMethodField()
+    time_before=serializers.SerializerMethodField()
+    content= serializers.SerializerMethodField()
 
     def get_comment_count(self,obj):
         return obj.comments_aticle.count()
-
+    
+    def get_content(self, obj):
+        answer = list(obj.content)
+        if len(answer) > 20:
+            return ''.join(answer[0:20])
+        else:
+            return obj.content
 
 
     author = serializers.ReadOnlyField(
@@ -25,10 +35,36 @@ class ArticleSerializer(serializers.ModelSerializer):
             "author",
             "created_at",
             "comment_count",
+            "time_before",
 
         ]
         read_only_fields = ('created_at', )
 
+# 게시물 작성시간을 계산해서 단위에 맞는 형식으로 반환
+    def get_time_before(self,obj):
+        now = datetime.now()
+        delta = datetime.now(tz=timezone.utc)- obj.created_at
+
+        if delta.days >= 365:
+            years = delta.days // 365
+            return f"{years}년 전"
+        elif delta.days >= 30:
+            months = delta.days // 30
+            return f"{months}달 전"
+        elif delta.days >= 7:
+            weeks = delta.days // 7
+            return f"{weeks}주 전"
+        elif delta.days >= 1:
+            return f"{delta.days}일 전"
+        elif delta.seconds >= 3600:
+            hours = delta.seconds // 3600
+            return f"{hours}시간 전"
+        elif delta.seconds >= 60:
+            minutes = delta.seconds // 60
+            return f"{minutes}분 전"
+        else:
+            return "방금 전"   
+        
 
 # 댓글 목록 시리얼라이저
 class CommentSerializer(serializers.ModelSerializer):
