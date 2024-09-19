822af2b4016a3b913f3a58925bbb521a1c665292
SHY-89 <tjduwkrn@gmail.com>
2024-09-19 23:19:16 +0900

    modi access_token time modify

--- spartanews/settings.py
+++ spartanews/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
 AUTH_USER_MODEL = "accounts.User"
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=100),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "BLACKLIST_AFTER_ROTATION": True,
 }
