5a5f76234ae1a21788e77581d9c131e54e4acb08
SHY-89 <tjduwkrn@gmail.com>
2024-09-19 23:21:25 +0900

    feat: token_refresh add

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenBlacklistView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenBlacklistView, TokenRefreshView
 from . import views
 
 
 app_name= 'accounts'
 urlpatterns = [
     path("login/",  TokenObtainPairView.as_view()),
+    path("token/refresh/", TokenRefreshView.as_view()),
     path("signup/", views.Signup.as_view()),
     path("logout/", TokenBlacklistView.as_view()),
     path('profile/<str:username>/', views.UserProfileView.as_view()),
